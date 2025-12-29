c9229af204573a92e5a1f262fd75b85a7232b8c9
Sasyk Kirill Vasilevich <sasyk@equip.me>
2025-12-29 13:02:52 +0300

    Добавил линты

--- Makefile
+++ Makefile
@@ -10,3 +10,6 @@ validate:
 
 autoload:
 	composer dump-autoload
+
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace Hexlet\Code;
 
 use function cli\line;
 use function cli\prompt;
 
-class Cli {
-    public static function greetings() {
+class Cli
+{
+    public static function greetings()
+    {
         line('Welcome to the Brain Game!');
         $name = prompt('May I have your name?');
         line("Hello, %s!", $name);
